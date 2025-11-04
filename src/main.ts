aa3e12dac87b790d99ed7d8ba402209e532e475f
Tung Nguyen <tung.ht.nguyen@pkh.vn>
2025-11-04 09:51:18 +0700

    update main

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,17 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
+  app.enableCors({
+    origin: [
+      'http://localhost:3000',
+      'http://192.168.1.4:3000', // IP LAN của bạn (để mobile truy cập)
+      'http://192.168.0.130:3000', // IP LAN của bạn (để mobile truy cập)
+      'capacitor://localhost', // nếu build thành mobile app (Capacitor)
+      'http://localhost', // nếu chạy app mobile với local dev server
+    ],
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    credentials: true,
+  });
   await app.listen(3009);
 }
 bootstrap();
