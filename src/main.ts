1bbb14dcbe4ff3b1a300c6772e883af1dbdd8301
Tung Nguyen <tung.ht.nguyen@pkh.vn>
2025-11-10 11:33:20 +0700

    update main

--- src/main.ts
+++ src/main.ts
@@ -6,8 +6,10 @@ async function bootstrap() {
   app.enableCors({
     origin: [
       'https://holdev-ui.onrender.com',
+      'https://holdev-ui.vercel.app/',
       'http://localhost:3000',
       'http://192.168.1.4:3000', // IP LAN của bạn (để mobile truy cập)
+      'http://10.30.0.248:3000', // IP LAN của bạn (để mobile truy cập)
       'http://192.168.1.8:3000', // IP LAN của bạn (để mobile truy cập)
       'http://192.168.0.130:3000', // IP LAN của bạn (để mobile truy cập)
       'capacitor://localhost', // nếu build thành mobile app (Capacitor)
